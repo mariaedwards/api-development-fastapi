a20ca9cbd788e5f6e811d869771239bc5b31e7bc
maria.edwards <maria.ana.edwards@gmail.com>
2023-01-07 14:32:27 -0800

    Changes doc

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -47,7 +47,7 @@ def create_post(post: schemas.PostCreate, db: Session = Depends(get_db),
 @router.get("/{post_id}", response_model=schemas.PostResponse)
 def get_post(post_id: int, db: Session = Depends(get_db),
              _current_user: dict = Depends(oauth2.get_current_user)):
-    """ Creates a post by id
+    """ Gets a post by id
     """
     post = db.query(models.Post).filter(models.Post.id == post_id).first()
     if not post:
