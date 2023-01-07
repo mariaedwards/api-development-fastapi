19d337bbff29368c4c4634819cc7d922b08bfd54
maria.edwards <maria.ana.edwards@gmail.com>
2023-01-07 14:30:46 -0800

    Changes .env access

--- app/config.py
+++ app/config.py
@@ -1,20 +1,23 @@
 """ Setting and type checking of env variables
 """
-import os
 from pydantic import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
 
 
 class Settings(BaseSettings):
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
-    ALGORITHM: str = os.getenv("ALGORITHM")
-    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
-    POSTGRES_HOST: str = os.getenv("POSTGRES_HOST")
-    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
-    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
-    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    """Env variables
+    """
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    ALGORITHM: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_USER: str
+    SECRET_KEY: str
+
+    class Config:
+        """Env variables source
+        """
+        env_file = ".env"
 
 
 settings = Settings()
