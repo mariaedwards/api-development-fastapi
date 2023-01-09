dc4e95da44528ae7adc3b2609a08b8a8dba12935
maria.edwards <maria.ana.edwards@gmail.com>
2023-01-08 18:41:30 -0800

    Adds CORS middleware

--- app/main.py
+++ app/main.py
@@ -2,10 +2,22 @@
 """
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import posts, users, auth, likes
 
+#  public API
+# TODO Update for security
+origins = ["*"]
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(posts.router)
 app.include_router(users.router)
